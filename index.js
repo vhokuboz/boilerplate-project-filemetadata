15a890c1a63ad6e5a254202b9d4ca7b51d891653
Okubo <vitor.okubo@outlook.com>
2025-08-27 17:59:52 -0300

    Projeto resolvido

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +12,18 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+const cpUpload = upload.single('upfile')
+app.post('/api/fileanalyse', cpUpload, function (req, res, next) {
+  const file = req.file;
+
+  const responseJson = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  }
+  res.send(responseJson);
+})
 
 
 
